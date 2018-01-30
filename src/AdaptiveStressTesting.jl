256120cf6eca09f14a4c0fd0272529dcc2600c44
Ritchie Lee <ritchie.lee@gmail.com>
2018-01-29 22:25:39 -0800

    fix more 0.6 depwarns

--- src/AdaptiveStressTesting.jl
+++ src/AdaptiveStressTesting.jl
@@ -54,7 +54,7 @@ import Base: hash, isequal, ==
 const DEFAULT_RSGLENGTH = 3
 const G_RNG = MersenneTwister(0) #not used
 
-type ASTParams
+mutable struct ASTParams
     max_steps::Int64 # safety for runaways in sim
     rsg_length::Int64 # dictates number of unique available random seeds
     init_seed::Int64 # initial value of seed on construct
@@ -62,7 +62,7 @@ type ASTParams
 end
 ASTParams() = ASTParams(0, DEFAULT_RSGLENGTH, 0, nothing)
 
-type AdaptiveStressTest
+mutable struct AdaptiveStressTest
     params::ASTParams
     sim
     sim_hash::UInt64 #keeps the sim in sync
@@ -98,12 +98,12 @@ type AdaptiveStressTest
     end
 end
 
-type ASTAction <: Action
+mutable struct ASTAction <: Action
     rsg::RSG
 end
 ASTAction(len::Int64=DEFAULT_RSGLENGTH, seed::Int64=0) = ASTAction(RSG(len, seed))
 
-type ASTState <: State
+mutable struct ASTState <: State
     t_index::Int64 #sanity check that at least the time corresponds
     hash::UInt64 #hash sim state to match with ASTState
     parent::Union{Void,ASTState} #parent state, root=nothing
